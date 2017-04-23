d69816e1489d0dcdde02eb2b4fc4e1dedd3a5ee7
eramosr17 <eramosr14@gmail.com>
2017-04-23 12:38:03 -0400

    Initial list of files

---
+++ Helper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Data.Entity;
+using System.Data.Entity.Design.PluralizationServices;
+
+namespace AbpOven
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Return the Project folder given the Project name
+        /// </summary>
+        /// <param name="projectName"></param>
+        /// <returns>Path to project name</returns>
+        public static string GetProjectDirectory(string projectName)
+        {
+            string path = Directory.GetCurrentDirectory();
+            var idx = path.IndexOf(@"AbpOven", StringComparison.InvariantCultureIgnoreCase);
+            path = path.Substring(0, idx);
+            var projPath = Path.Combine(path, projectName);        
+            return projPath;
+        }
+
+        /// <summary>
+        /// Get the project file name given the project name
+        /// </summary>
+        /// <param name="projectName"></param>
+        /// <returns>Return the path to the project.csproj file</returns>
+        public static string GetProjectFileName(string projectName)
+        {
+            var projPath = GetProjectDirectory(projectName);
+            var projFile = Path.Combine(projPath , projectName + @".csproj");
+            return projFile;
+        }
+
+        /// <summary>
+        /// Pluralize the name of an element
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static string PluralizeElement(string element)
+        {
+            var service = PluralizationService.CreateService(CultureInfo.GetCultureInfo("en-us"));
+            return service.Pluralize(element);
+        }
+
+        public static string GetTemplatesDirectory()
+        {
+            return GetProjectDirectory(@"AbpOven\\Templates");
+        }
+
+        public static void AddFileToProject(List<string> fileList, string projectName)
+        {            
+            var p = new Microsoft.Build.Evaluation.Project(projectName);
+            foreach (var fileN in fileList)
+            {
+                p.AddItem("Compile", fileN);
+                p.Save();
+            }
+            
+        }
+
+        public static string GetCoreProjectPath(string arg)
+        {
+            var result = arg.Split('.');
+            if (result.Length < 2)
+            {
+                throw new Exception("Project Name and element cannot contain . symbol");
+            }
+            var projectName = result[0] + @".Core";
+            return GetProjectFileName(projectName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbpOven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 1)
+            {
+                PrintHelp();
+            }
+            else
+            {
+                switch (args[0].ToLower())
+                {
+                    case "-c":
+                        CreateCommand(args);
+                        break;
+
+                    default:
+                        PrintHelp();
+                        break;
+                }
+            }
+
+           
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("USAGE: <command> <element> <ProjectName.[folderName.]itemName>");
+            Console.WriteLine("<command>: " +
+                              "c : Create \r\n" +
+                              "<element>:\r\n" +
+                              "  e: Entity \r\n" +
+                              "  s: Service \r\n" +
+                              "  v: View \r\n" +
+                              "  t: Test \r\n" +
+                              "  full: Create an entity, service, test, view" +
+                              "note: Every element will be created in the respective project folder");
+        }
+
+        static void CreateCommand(string[] args)
+        {
+
+            switch (args[1].ToLower())
+            {
+                //syntax: abpOven -c e BaseWebsite.Domain
+                case "e":
+                    var projectPath = Helper.GetCoreProjectPath(args[2]); 
+                    CommandFunction cmFunction =new CommandFunction(args[2]);
+                    var file = cmFunction.CreateEntity();
+                    var fileList = new List<string>() {file};
+                    Helper.AddFileToProject(fileList,projectPath);
+                    break;
+                case "s":
+                    throw new NotImplementedException("Coming soon!!");
+                    break;
+                case "v":
+                    throw new NotImplementedException("Coming soon!!");
+                    break;
+                case "t":
+                    throw new NotImplementedException("Coming soon!!");
+                    break;
+                case "full":
+                    throw new NotImplementedException("Coming soon!!");
+                    break;
+                default: PrintHelp();
+                    break;
+            }
+        }
+
+    }
+}
